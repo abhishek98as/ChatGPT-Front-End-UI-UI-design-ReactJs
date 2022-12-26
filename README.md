6827bacb8c1690cce3e9816988fe594dd33342f6
ABHISHEK SINGH <49355119+abhishek98as@users.noreply.github.com>
2022-12-26 13:51:08 +0530

    CHATgpt Project

---
+++ README.md
@@ -0,0 +1,14 @@
+# GPT3 Website
+
+## Concepts Learned
+- [x] Website Design / canva and figma
+- [x] BEM(Block-Element-Modifier) CSS
+- [x] Flexbox and Grid
+- [x] CSS Animation (**Gradients and Key-frames**)
+- [x] CSS Variables in React Project
+- [x] React Component Reusability
+- [x] Clean Imports in React Components
+- [x] React Project Structure
+- [x] Responsive Website using Reactjs
+- [x] Deploying React App to GitHub/netlify
+
---
+++ src/containers/possiblity/Possibility.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./possibility.css"
+
+import  possibilityImage  from '../../assets/possibility.png'
+
+const Possibility = () => {
+  return (
+    <div className="gpt3__possibility section__padding" id='possibility'>
+        <div className="gpt3__possibility-image">
+          <img src={possibilityImage} alt="possibility" />
+        </div>
+        <div className="gpt3__possibility-container">
+          <p><a href="/">Request Early Access to Get Started</a></p>
+          <h1>The possibilities are beyond your imagination.</h1>
+          <p>Yet be any for travelling assistance unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Possibility
